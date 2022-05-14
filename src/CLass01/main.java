a1896ccba9f0a8534dd4e71769b75715a1fc325b
vladajanca <vladajanca@gmail.com>
2022-05-14 12:34:31 -0400

    3rd commit

--- src/CLass01/main.java
+++ src/CLass01/main.java
@@ -5,5 +5,7 @@ public class main {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+
+        System.out.println("I am correct code");
     }
 }
